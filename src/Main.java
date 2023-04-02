411ea7514168365a4a3b3f1589f9ef86feca702b
Slava <q20142015@yandex.ru>
2023-04-02 23:49:12 +0300

    gazprom

--- src/Main.java
+++ src/Main.java
@@ -2,25 +2,25 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        Company Gazprom=new Company();
+        Company gazprom=new Company();
         for (int i=0;i<180;i++){
             Operator operator=new Operator();
-            Gazprom.hire(operator);
+            gazprom.hire(operator);
         }
         for (int i=0;i<100;i++){
             Manager manager=new Manager();
-            Gazprom.hire(manager);
+            gazprom.hire(manager);
         }
         for (int i=0;i<10;i++){
             TopManager topmanager=new TopManager();
-            Gazprom.hire(topmanager);
+            gazprom.hire(topmanager);
         }
-        Gazprom.payBonuses();
+        gazprom.payBonuses();
 
         ArrayList< Employee > Emin = new ArrayList< Employee >();
         ArrayList< Employee > Emax = new ArrayList< Employee >();
-        Emin=Gazprom.getLowestSalaryStaff(5);
-        Emax=Gazprom.getTopSalaryStaff(5);
+        Emin=gazprom.getLowestSalaryStaff(5);
+        Emax=gazprom.getTopSalaryStaff(5);
 
 
         int pause=0;
